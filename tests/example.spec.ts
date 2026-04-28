8ba327f614cf8c6a3e78655ae8f4730bb412ffbd
pradeeptest2207 <pradeepkumar2207@outlook.com>
2026-04-28 14:55:18 +1000

    change1.1

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -13,6 +13,5 @@ test('get started link', async ({ page }) => {
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  
 });
