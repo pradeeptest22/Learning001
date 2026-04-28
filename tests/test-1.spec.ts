64fc6c799da61d51991211a9bb5fc8da66f39ffb
pradeeptest2207 <pradeepkumar2207@outlook.com>
2026-04-28 16:23:19 +1000

    commit2

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://practicesoftwaretesting.com/auth/login');
+  await page.locator('[data-test="email"]').fill('testplaywright_202602@mailinator.com');
+  await page.locator('[data-test="password"]').fill('Testing@1708');
+  await page.locator('[data-test="login-submit"]').click();
+  await expect(page.locator('[data-test="nav-invoices"]')).toContainText('Invoices');
+  await expect(page.locator('[data-test="nav-messages"]')).toContainText('Messages');
+  await page.locator('[data-test="language-select"]').click();
+  await page.locator('[data-test="nav-home"]').click();
+});
